16a3381f7018e7458cb483aae3e0b8361f7f8039
damir <damir.danzo@gmail.com>
2024-04-17 15:08:53 +0600

    first commit

---
+++ src/main/kotlin/kg/damir/face/FaceApplication.kt
@@ -0,0 +1,11 @@
+package kg.damir.face
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FaceApplication
+
+fun main(args: Array<String>) {
+	runApplication<FaceApplication>(*args)
+}
